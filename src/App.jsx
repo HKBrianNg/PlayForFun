bc0a631ac8239d68fcd6ace3822065cb43fca176
HKBrianNg <ngshunchiang@hotmail.com>
2026-05-09 21:41:27 +0800

    第一次提交

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import {Space} from 'antd'
+
+function App() {
+
+  return (
+    <div>
+      <Space size='large'>
+        <h1>Brian</h1>
+        <h1>Diana</h1>
+      </Space>
+    </div>
+  )
+}
+
+export default App
